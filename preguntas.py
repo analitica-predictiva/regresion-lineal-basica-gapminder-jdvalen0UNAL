922db359b7b7479ea3dbc287c13395d05ec9cd4d
Juan D. Valencia <jdvalen0@unal.edu.co>
2023-11-26 12:25:00 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -8,19 +8,18 @@ En este laboratio se construirá un modelo de regresión lineal univariado.
 import numpy as np
 import pandas as pd
 
-
 def pregunta_01():
     """
     En este punto se realiza la lectura de conjuntos de datos.
     Complete el código presentado a continuación.
     """
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("gm_2008_region.csv")
+    df = pd.read_csv ("gm_2008_region.csv")
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df["life"].values
-    X = df["fertility"].values
-
+    y = df['life'].to_numpy()
+    X = df['fertility'].to_numpy()
+    
     # Imprima las dimensiones de `y`
     print(y.shape)
 
@@ -28,14 +27,14 @@ def pregunta_01():
     print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = y.reshape(-1, 1)
+    y_reshaped = y.reshape(-1,1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = X.reshape(-1, 1)
+    X_reshaped = X.reshape(-1,1)
 
     # Imprima las nuevas dimensiones de `y`
     print(y_reshaped.shape)
-
+    
     # Imprima las nuevas dimensiones de `X`
     print(X_reshaped.shape)
 
@@ -47,25 +46,23 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("C:\\Users\\jvalencia\\OneDrive - Elico Group S.A.S\\Documentos\\GitHub\\regresion-lineal-basica-gapminder-jdvalen0UNAL\\gm_2008_region.csv")
+    df = pd.read_csv ('gm_2008_region.csv')
 
     # Imprima las dimensiones del DataFrame
     print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    correlation_life_fertility = round(df[['life', 'fertility']].corr().loc['life', 'fertility'], 4)
-    print(correlation_life_fertility)
+    print(df['life'].corr(df['fertility']).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    mean_life = round(df['life'].mean(), 4)
-    print(mean_life)
+    print(df['life'].mean().round(4))
 
     # Imprima el tipo de dato de la columna `fertility`.
     print(type(df['fertility']))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    correlation_GDP_life = round(df[['GDP', 'life']].corr().loc['GDP', 'life'], 4)
-    print(correlation_GDP_life)
+    print(df['GDP'].corr(df['life']).round(4))
+
 
 def pregunta_03():
     """
@@ -74,32 +71,33 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("C:\\Users\\jvalencia\\OneDrive - Elico Group S.A.S\\Documentos\\GitHub\\regresion-lineal-basica-gapminder-jdvalen0UNAL\\gm_2008_region.csv")
-
+    df = pd.read_csv("gm_2008_region.csv")
+    
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df['fertility'].values.reshape(-1, 1)
-
+    X_fertility = df['fertility'].to_numpy()
+       
     # Asigne a la variable los valores de la columna `life`
-    y_life = df['life'].values
+    y_life = df['life'].to_numpy()
 
+    
     # Importe LinearRegression
     from sklearn.linear_model import LinearRegression
 
     # Cree una instancia del modelo de regresión lineal
     reg = LinearRegression()
 
-    # Cree el espacio de predicción. Esto es, use linspace para crear
+    # Cree El espacio de predicción. Esto es, use linspace para crear
     # un vector con valores entre el máximo y el mínimo de X_fertility
-    prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1, 1)
+    prediction_space = np.linspace(X_fertility.min(),X_fertility.max(),100).reshape(-1,1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(X_fertility, y_life)
+    reg.fit(X_fertility.reshape(-1,1), y_life.reshape(-1,1))
 
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
-    print(reg.score(X_fertility, y_life).round(4))
+    print(reg.score(X_fertility.reshape(-1,1), y_life.reshape(-1,1)).round(4))
 
 
 def pregunta_04():
@@ -109,39 +107,40 @@ def pregunta_04():
     """
 
     # Importe LinearRegression
-    # Importe train_test_split
-    # Importe mean_squared_error
     from sklearn.linear_model import LinearRegression
+    # Importe train_test_split
     from sklearn.model_selection import train_test_split
+    # Importe mean_squared_error  
     from sklearn.metrics import mean_squared_error
+
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("C:\\Users\\jvalencia\\OneDrive - Elico Group S.A.S\\Documentos\\GitHub\\regresion-lineal-basica-gapminder-jdvalen0UNAL\\gm_2008_region.csv")
+    df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df['fertility'].values.reshape(-1, 1)
+    X_fertility = df['fertility'].to_numpy()
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = df['life'].values
+    y_life = df['life'].to_numpy()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
     (X_train, X_test, y_train, y_test,) = train_test_split(
-        X_fertility,
-        y_life,
+        X_fertility.reshape(-1,1),
+        y_life.reshape(-1,1),
         test_size=0.2,
         random_state=53,
     )
 
     # Cree una instancia del modelo de regresión lineal
     linearRegression = LinearRegression()
-
+    
     # Entrene el clasificador usando X_train y y_train
     linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
     y_pred = linearRegression.predict(X_test)
-
+    
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    rmse = np.sqrt(mean_squared_error(y_pred, y_test))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
